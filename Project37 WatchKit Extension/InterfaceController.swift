3eae6247f58fc1c5206b825ba59e7614c37b8a3b
jslusser <jslusser@gmail.com>
2017-08-29 12:49:11 -0400

    Initial commit

---
+++ Project37 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,53 @@
+//
+//  InterfaceController.swift
+//  Project37 WatchKit Extension
+//
+//  Created by James Slusser on 8/23/17.
+//  Copyright © 2017 James Slusser. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import WatchConnectivity
+
+
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    @IBOutlet var welcomeText: WKInterfaceLabel!
+    @IBOutlet var hideButton: WKInterfaceButton!
+    @IBAction func hideWelcomeText() {
+        welcomeText.setHidden(true)
+        hideButton.setHidden(true)
+    }
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        
+        if WCSession.isSupported() {
+            let session = WCSession.default()
+            session.delegate = self
+            session.activate()
+        }
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        WKInterfaceDevice().play(.click)
+    }
+
+
+}
---
+++ Project37/CardViewController.swift
@@ -0,0 +1,96 @@
+//
+//  CardViewController.swift
+//  Project37
+//
+//  Created by James Slusser on 8/23/17.
+//  Copyright © 2017 James Slusser. All rights reserved.
+//  https://www.hackingwithswift.com/read/37/overview
+//
+
+import GameplayKit
+import UIKit
+
+class CardViewController: UIViewController {
+    
+    weak var delegate: ViewController!
+    
+    var front: UIImageView!
+    var back: UIImageView!
+    
+    var isCorrect = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.bounds = CGRect(x: 0, y: 0, width: 100, height: 140)
+        front = UIImageView(image: UIImage(named: "cardBack"))
+        back = UIImageView(image: UIImage(named: "cardBack"))
+        
+        view.addSubview(front)
+        view.addSubview(back)
+        
+        front.isHidden = true
+        back.alpha = 0
+        
+        UIView.animate(withDuration: 0.2) {
+            self.back.alpha = 1
+        }
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(cardTapped))
+        back.isUserInteractionEnabled = true
+        back.addGestureRecognizer(tap)
+
+        perform(#selector(wiggle), with: nil, afterDelay: 1)
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func cardTapped() {
+        delegate.cardTapped(self)
+    }
+
+    func wasntTapped() {
+        UIView.animate(withDuration: 0.7) {
+            self.view.transform = CGAffineTransform(scaleX: 0.00001, y: 0.00001)
+            self.view.alpha = 0
+        }
+    }
+    
+    func wasTapped() {
+        UIView.transition(with: view, duration: 0.7, options: [.transitionFlipFromRight], animations: { [unowned self] in
+            self.back.isHidden = true
+            self.front.isHidden = false
+        })
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func wiggle() {
+        if GKRandomSource.sharedRandom().nextInt(upperBound: 4) == 1 {
+            UIView.animate(withDuration: 0.2, delay: 0, options: .allowUserInteraction, animations: {
+                self.back.transform = CGAffineTransform(scaleX: 1.01, y: 1.01)
+            }) { _ in
+                self.back.transform = CGAffineTransform.identity
+            }
+            
+            perform(#selector(wiggle), with: nil, afterDelay: 8)
+        } else {
+            perform(#selector(wiggle), with: nil, afterDelay: 2)
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
